5297ad353eefdc3a72b38e5f7c5c1aa8c3e647a5
daeun2036 <61063249+daeun2036@users.noreply.github.com>
2020-10-11 17:13:46 +0900

    Update Form1.cs

--- Test/Form1.cs
+++ Test/Form1.cs
@@ -19,7 +19,7 @@ namespace Test
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("test");
+            MessageBox.Show("server");
         }
     }
 }
